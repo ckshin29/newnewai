74e357a125bed9e0c7b09cef3c1018b71a7233ba
ckshin29 <98381455+ckshin29@users.noreply.github.com>
2022-02-07 17:58:50 +0900

    3

---
+++ test.py
@@ -0,0 +1,23 @@
+#print()
+
+print('Hello Python Programming')
+print('52')
+print(1,2,3,4,5)
+
+print('한글사용') #주석은 코드에 대한 설명을 표현한다
+
+#기본 자료형
+#문자열 string '안녕' 'hello'
+#숫자 int 1,2,3,4,5
+#논리(불) boolean True,False
+
+print(type('안녕'))
+print(type('안녕'))
+print(type(123))
+print('"안녕"이라고 했다')
+print("'안녕'이라고 했다'")
+print("\"안녕\"이라고 했다")
+
+print('이름\t나이\t지역')
+print('김현아\t20\t강남구')
+print('오철수\t25\t서초구')
